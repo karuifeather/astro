fe667168521369cd144648a73466f2f1d15c792b
karuifeather <aryalaashaya@gmail.com>
2021-03-12 16:04:19 +0545

    refactor components

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,26 @@
 import styles from './footer.module.scss';
+import Link from 'next/link';
 
-const Footer = () => {
+interface FooterProps {
+  home?: boolean;
+}
+
+const Footer = ({ home }: FooterProps) => {
   return (
     <footer className={styles.footer}>
-      <a href='#' target='_blank' className={styles.footer__link}>
-        Created by mE
-      </a>
+      {home ? (
+        <a href='#' target='_blank' className={styles.footer__link}>
+          Created by mE for his Next.js practice. No copyright intended.
+        </a>
+      ) : (
+        <a
+          href='https://blog.prepscholar.com/'
+          target='_blank'
+          className={styles.footer__link}
+        >
+          All the article were scraped from PrepScholar.
+        </a>
+      )}
     </footer>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import Head from 'next/head';
+import Layout from '../components/Layout';
 import Main from '../components/Main';
 
 function HomePage() {
   return (
-    <div className='container'>
-      <Navbar />
-      <Main />
-      <Footer />
-    </div>
+    <Layout home={true}>
+      <Head>
+        <title>Astro | Find yourself</title>
+      </Head>
+      <Main></Main>
+    </Layout>
   );
 }
 
